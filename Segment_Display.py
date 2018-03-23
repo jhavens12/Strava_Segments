de3a6ca55d4519ec13edb79dd32bd54b3d0f05db
jhavens <jhavens12@gmail.com>
2018-03-23 14:57:09 -0400

    update

--- Segment_Display.py
+++ Segment_Display.py
@@ -83,14 +83,15 @@ def set_button_titles(v,old_dict):
     #need button dict of name of segment and ID of segment
     if len(old_dict.keys()) > 11:
         for n,segment in enumerate(old_dict): #limit to number of labels somehow
-            button_name = 'button'+str(n)
-            label_title = str(old_dict[segment]['information']['name'])
-            button_dict[label_title] = segment #save button with segment id
-            v[button_name].title = label_title #set titles for buttons
-            v[button_name].action = seg_button_pressed
-            if n == 12:
+            if n >= 12:
                 print("There are too many starred segments")
-                break
+            else:
+                button_name = 'button'+str(n)
+                label_title = str(old_dict[segment]['information']['name'])
+                button_dict[label_title] = segment #save button with segment id
+                v[button_name].title = label_title #set titles for buttons
+                v[button_name].action = seg_button_pressed
+
     # else:
     #     for n,segment in enumerate(old_dict): #limit to number of labels somehow
     #         button_name = 'button'+str(n)
