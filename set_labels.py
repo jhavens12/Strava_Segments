7a3c01dd1224a15c7b403f1c762a6b3902c8f1d1
jhavens <jhavens12@gmail.com>
2018-03-23 10:20:41 -0400

    label update

--- set_labels.py
+++ set_labels.py
@@ -16,6 +16,7 @@ def set_200_series(v,old_dict,segment):
     if 'Jonathan_old' in old_dict[segment]:
         print("There is an old record for "+str(old_dict[segment]['information']['name']))
         #v['label300'].text = str(old_dict[segment]['Jonathan_old']['hms'])
+        v['label300'].text = str("UPDATED")
         #v['label301'].text = str(old_dict[segment]['Jonathan_old']['start_time'])
         #v['label302'].text = str(old_dict[segment]['Jonathan_old']['rank'])
         #v['label303'].text = str(old_dict[segment]['information']['athlete_segment_stats']['effort_count'])
